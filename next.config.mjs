ccf1ca182dba66389431661ab1a8e4d4f5f76db1
alaminislam3203 <alaminislam3203@gmail.com>
2025-11-27 10:35:18 +0600

    deploy to netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
+
   images: {
-    domains: [
-      'scontent.fspd3-1.fna.fbcdn.net',
-      'scontent.fspd6-1.fna.fbcdn.net',
-      'lh3.googleusercontent.com',
-      'raw.githubusercontent.com',
-      'i.postimg.cc',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'scontent.fspd3-1.fna.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.fspd6-1.fna.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      },
     ],
   },
 };
