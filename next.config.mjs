cb5e9992e9aacd72d0f0bfd4fdc142e137443f46
alaminislam3203 <alaminislam3203@gmail.com>
2025-11-27 09:37:44 +0600

    basic next.js merxomart  project structure added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    domains: [
+      'scontent.fspd3-1.fna.fbcdn.net',
+      'scontent.fspd6-1.fna.fbcdn.net',
+      'lh3.googleusercontent.com',
+      'raw.githubusercontent.com',
+      'i.postimg.cc',
+    ],
+  },
 };
 
 export default nextConfig;
